bd8dfe7bc3246cdd4b8c2d7bbf22f68317903c54
David Troy <dave@popvox.com>
2009-01-11 10:43:38 -0500

    Final changes

--- Classes/RegistrationView.m
+++ Classes/RegistrationView.m
@@ -350,7 +350,7 @@
 			else
 			{
 				// this cell hosts the info on where to find the code
-				((SourceCell *)sourceCell).sourceLabel.text = @"Please enter your email address.";
+				((SourceCell *)sourceCell).sourceLabel.text = @"Please enter your email address";
 			}
 			break;
 		case 4: //Zip
